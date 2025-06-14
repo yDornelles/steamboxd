f6abbef82cec8aff9d67840fed4d2ed456c8524b
Gabriel Dornelles <gabrieldornellesmachado@gmail.com>
2025-06-14 15:30:18 -0300

    number four

--- backend/models/jogo.py
+++ backend/models/jogo.py
@@ -1,7 +1,7 @@
 class Jogo:
     def __init__(self, id_igdb, nome, resumo=None, data_lancamento=None,
                  generos=None, plataformas=None, nota=None, status="quero_jogar",
-                 comentario=None, capa_id=None, usuario_id=None):
+                 comentario=None, capa_id=None, usuario_id=None, favorito=False):
         self.id_igdb = id_igdb
         self.nome = nome
         self.resumo = resumo
@@ -13,6 +13,7 @@ class Jogo:
         self.comentario = comentario
         self.capa_id = capa_id
         self.usuario_id = usuario_id
+        self.favorito = favorito
 
     def salvar(self, db):
         cursor = db.cursor()
@@ -20,8 +21,8 @@ class Jogo:
             INSERT INTO jogos (
                 id_igdb, nome, resumo, data_lancamento,
                 generos, plataformas, nota, status,
-                comentario, capa_id, usuario_id
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                comentario, capa_id, usuario_id, favorito
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
             self.id_igdb,
             self.nome,
@@ -33,6 +34,7 @@ class Jogo:
             self.status,
             self.comentario,
             self.capa_id,
-            self.usuario_id
+            self.usuario_id,
+            self.favorito 
         ))
         db.commit()
